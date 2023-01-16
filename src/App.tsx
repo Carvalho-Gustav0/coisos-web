e861d47096ab0c02be14210d900c45ede02c93e3
Gustavo Carvalho <gustavo.carvalho@wansoft.com.br>
2023-01-16 15:40:58 -0300

    Adiciona uso de Context no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,37 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/home"
 import Rpg from "./pages/rpg";
 import ItemMenu from './components/menu/items';
+import { UserContextProvider } from './contexts/UserContext';
 
 export default function App() {
   return (
     <Styled.App>
       <BrowserRouter>
-        
-        <Styled.Container>
-          <Styled.Header>
-              <Link to={'/'} style={{ textDecoration: 'none' }}>
-                <Styled.Title>
-                  Coisos
-                </Styled.Title>
-              </Link>
-          </Styled.Header>
-          
-          <Styled.Menu>
+        <UserContextProvider>
+          <Styled.Container>
+            <Styled.Header>
+                <Link to={'/'} style={{ textDecoration: 'none' }}>
+                  <Styled.Title>
+                    Coisos
+                  </Styled.Title>
+                </Link>
+            </Styled.Header>
+            
+            <Styled.Menu>
                 <ItemMenu path={'/home'} name={'Home'}/>
                 <ItemMenu path={'/rpg'} name={'Rpg'}/>
                 <ItemMenu path={'/arquivos'} name={'Arquivos'}/>
-          </Styled.Menu>
-        </Styled.Container>
+            </Styled.Menu>
+          </Styled.Container>
 
-        <Routes>
-          {["/home", "/"].map((value, index) => 
-            <Route key={index} path={value} element={<Home />}/>
-          )}
-          <Route path="/rpg" element={<Rpg />} />
-        </Routes>
-
+          <Routes>
+            {["/home", "/"].map((value, index) => 
+              <Route key={index} path={value} element={<Home />}/>
+            )}
+            <Route path="/rpg" element={<Rpg />} />
+          </Routes>
+          
+        </UserContextProvider>
       </BrowserRouter>
     </Styled.App>
   )
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode,useState } from "react";
 
 interface UserContextPropsType {
-    props: ReactNode;
+    children: ReactNode;
+}
+
+interface UserType {
+    name: string;
+    identifier: string;
+    password: string;
 }
 
 interface UserContextType {
@@ -13,6 +19,8 @@ interface UserContextType {
     setUserPassword: (newState: string) => void,
     userToken: string,
     setUserToken: (newState: string) => void,
+    user: UserType,
+    setUser: (newState: UserType) => void,
 }
 
 const initialValue = {
@@ -24,21 +32,27 @@ const initialValue = {
     setUserPassword: () => {},
     userToken: '',
     setUserToken: () => {},
+    user: {
+        name: '',
+        identifier: '',
+        password: '',
+    },
+    setUser: () => {},
 }
 
 export const UserContext = createContext<UserContextType>(initialValue)
 
-export function UserContextProvider({props}: UserContextPropsType ) {
-
+export const UserContextProvider = ({children}: UserContextPropsType ) => {
     const [ userName, setUserName ] = useState<string>(initialValue.userName)
     const [ userIdentifier, setUserIdentifier ] = useState<string>(initialValue.userIdentifier)
     const [ userPassword, setUserPassword ] = useState<string>(initialValue.userPassword)
     const [ userToken, setUserToken ] = useState<string>(initialValue.userToken)
+    const [ user, setUser ] = useState<UserType>(initialValue.user)
 
     return (
         <UserContext.Provider value={{userName, setUserName, userIdentifier, setUserIdentifier,
-         userPassword, setUserPassword, userToken, setUserToken}}>
-            {props}
+         userPassword, setUserPassword, userToken, setUserToken, user, setUser}}>
+            {children}
         </UserContext.Provider>
     )
 }
