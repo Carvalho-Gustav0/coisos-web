7f5454c6c3f37e8147374dbe7d4298fde09d4259
Gustavo Carvalho <gustavo.csemail@gmail.com>
2023-07-03 16:08:16 -0300

    Extract app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import * as Styled from './style'
-import { Routes, Route } from "react-router-dom"
-import { Home } from "./pages/home"
-import { Rpg } from "./pages/rpg";
 import { UserContextProvider } from './contexts/UserContext'
-import { Archives } from './pages/archives'
 import { Header } from './components/header'
 import { ModalProvider } from 'styled-react-modal'
+import { AppRoutes } from './components/routes';
 
 export function App() {
 
@@ -15,12 +12,7 @@ export function App() {
         <Styled.App>
           <Header />
 
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/rpg" element={<Rpg />} />
-            <Route path="/arquivos" element={<Archives />} />
-            <Route path="*" element={<>Página não encontrada</>} />
-          </Routes>
+          <AppRoutes />
         </Styled.App>
       </ModalProvider>
     </UserContextProvider >
---
+++ src/components/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../../pages/home";
+import { Rpg } from "../../pages/rpg";
+import { Archives } from "../../pages/archives";
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/rpg" element={<Rpg />} />
+            <Route path="/arquivos" element={<Archives />} />
+            <Route path="*" element={<>Página não encontrada</>} />
+        </Routes>
+    )
+}
